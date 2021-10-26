591d0abe59c54b132347a493db2657024bd358aa
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2021-10-26 16:29:34 +0100

    Add players

--- app.rb
+++ app.rb
@@ -10,5 +10,13 @@ get '/' do
   "Testing infrastructure working!"
 end
 
+get '/start' do
+  erb(:index)
+end 
+
+post '/names' do 
+"#{params[:player1]} #{params[:player2]}"
+end 
+
 run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -5,6 +5,11 @@ feature "Infrastructure working?" do
     visit '/'
     expect(page).to have_content "Testing infrastructure working!"
   end
-
-
+  scenario "Enter two players name" do 
+    visit '/'
+    fill_in 'player1', with: 'player1'
+    fill_in 'player2', with: 'player2'
+  click_button "submit"
+  expect(page).to have_content 'player1 vs player2'
+  end 
 end
